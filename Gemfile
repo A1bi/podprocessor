a515afc767ae20a1f84bf1f9a29a0535de1d015b
Albrecht Oster <albrecht@oster.online>
2019-02-13 20:17:10 +0100

    add slim views and assets through sprockets

--- Gemfile
+++ Gemfile
@@ -3,5 +3,9 @@
 source 'https://rubygems.org'
 
 gem 'puma'
+gem 'sass'
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'slim'
+gem 'sprockets'
+gem 'uglifier'
--- app.rb
+++ app.rb
@@ -1,12 +1,35 @@
 # frozen_string_literal: true
 
+require 'sassc'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'slim'
+require 'sprockets'
+require 'uglifier'
 
-get '/' do
-  'Hello world!'
-end
+class PodProcessor < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-not_found do
-  'Unknown action.'
+  set :environment, Sprockets::Environment.new
+
+  environment.append_path 'assets'
+
+  environment.js_compressor  = :uglify
+  environment.css_compressor = :sass
+
+  # get assets
+  get '/assets/*' do
+    env['PATH_INFO'].sub!('/assets', '')
+    settings.environment.call(env)
+  end
+
+  get '/' do
+    slim :index
+  end
+
+  not_found do
+    'Unknown action.'
+  end
 end
