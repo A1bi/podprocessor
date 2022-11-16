c35d8f9f7d2462c81847bc640184793e8c9e1c60
Albrecht Oster <albrecht@oster.online>
2022-11-15 18:00:13 -0600

    Connect to Redis using socket

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+redis = { path: '/var/run/redis/redis.sock' }
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost/' }
+  config.redis = redis
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost/' }
+  config.redis = redis
 end
